c628835a4369d33ce7c3333e6d1a2e9a71f15050
novvais <eduardodepaulanovais10@gmail.com>
2026-06-01 21:50:55 -0300

    Fix: Resolve caminhos de rotas e atualiza navegacao global

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import Cardapio from "./pages/Cardapio.tsx";
-import Lanchonete from "./pages/Lanchonete.tsx";
-import MapaInterativo from "./pages/MapaInterativo.tsx";
-import NaPraia from "./pages/NaPraia.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import Index from "./presentation/components/pages/Index.tsx";
+import Cardapio from "./presentation/components/pages/Cardapio.tsx";
+import Lanchonete from "./presentation/components/pages/Lanchonete.tsx";
+import MapaInterativo from "./presentation/components/pages/MapaInterativo.tsx";
+import NaPraia from "./presentation/components/pages/NaPraia.tsx";
+import NotFound from "./presentation/components/pages/NotFound.tsx";
+import Checkout from "./presentation/components/pages/Checkout.tsx";
+import HistoricoPedidos from "./presentation/components/pages/HistoricoPedidos.tsx";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +26,8 @@ const App = () => (
           <Route path="/na-praia/lanchonete" element={<Lanchonete />} />
           <Route path="/na-praia/lanchonete/:restaurantId" element={<Cardapio />} />
           <Route path="/na-praia/mapa" element={<MapaInterativo />} />
+          <Route path="/na-praia/checkout" element={<Checkout />} />
+          <Route path="/na-praia/historico" element={<HistoricoPedidos />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
